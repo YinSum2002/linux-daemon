c79c8164ae2339613f596b5036eeb9c593618669
justin <chanj1421@gmail.com>
2026-02-08 15:46:56 -0500

    Not working on mac, going to test this on linux and see if it works

--- main.c
+++ main.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 
 #define BUFFER_SIZE 100
 
 int main() {
   char buffer[BUFFER_SIZE];
 
-  // Open /proc/stat in read mode
-  FILE* fp = fopen("/proc/stat", "r");
   while (1) {
-    // Read the first line using fgets()
-    if (fgets(buffer, BUFFER_SIZE, fp) != NULL) {
-      printf("Data from /proc/stat: %s", buffer);
+    // Open /proc/stat in read mode
+    FILE* fp = fopen("/proc/stat", "r");
+    if (fp == NULL){
+      perror("fopen failed");
+      sleep(1);
+      continue;
     } else {
-      printf("Count not read a line or file is empty.\n");
+      // Read the first line using fgets()
+      if (fgets(buffer, BUFFER_SIZE, fp) != NULL) {
+        printf("Data from /proc/stat: %s", buffer);
+      } else {
+        printf("Count not read a line or file is empty.\n");
+      }
+      fclose(fp);
+      sleep(1);
     }
-    sleep(1);
+    
   }
-
-  fclose(fp);
 
   return 0;
 }
