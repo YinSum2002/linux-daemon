b48466b653ca75b5dc5124ddb9646f95ba81c3b1
justin <chanj1421@gmail.com>
2026-02-09 10:51:07 -0500

    forgot to change types of CPU stats

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@ int parse_cpu_line(const char *line, struct CPUStatus *out){
         return -1;
     }
     char label[4];
-    if (sscanf(line, "%3s %d %d %d %d", label, &out->user, &out->nice, &out->system, &out->idle) == 5){
+    if (sscanf(line, "%3s %llu %llu %llu %llu", label, &out->user, &out->nice, &out->system, &out->idle) == 5){
         return 0;
     }
     return -1;
