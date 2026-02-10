e082505d0c8fe8420d076f2c46b9039b056adca7
justin <chanj1421@gmail.com>
2026-02-10 10:53:02 -0500

    fixed minor bug in parser

--- parser.c
+++ parser.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
 #include "parser.h"
 
-int parse_cpu_line(const char *line, struct CPUStatus *out){
-    if ((line == NULL) || (out == NULL)){
-        return -1;
-    }
-    char label[4];
-    if (sscanf(line, "%3s %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", label, &out->user, &out->nice, &out->system, &out->idle, &out->iowait, &out->irq, &out->softirq, &out->steal, &out->guest, &out->guest_nice) == 5){
-        return 0;
-    }
+#include <stdio.h>
+
+int parse_cpu_line(const char* line, struct CPUStatus* out) {
+  if ((line == NULL) || (out == NULL)) {
     return -1;
+  }
+  char label[4];
+  if (sscanf(line, "%3s %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu",
+             label, &out->user, &out->nice, &out->system, &out->idle,
+             &out->iowait, &out->irq, &out->softirq, &out->steal, &out->guest,
+             &out->guest_nice) == 11) {
+    return 0;
+  }
+  return -1;
 }
