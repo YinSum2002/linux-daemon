9cc527672412d499759cd8e4095d4ba5fb5c3aec
justin <chanj1421@gmail.com>
2026-02-25 11:01:43 -0500

    forgot to add an input to the threads

--- main.c
+++ main.c
@@ -111,7 +111,7 @@ int main() {
 
   // create the sampler and consumer threads(last argument is the argument for the function sampler_thread. For now it's NULL, later it will be changed to &mailBox)
   pthread_create(&sampler_tid, NULL, sampler_thread, &mailBox);
-  pthread_create(&consumer_tid, NULL, consumer_thread, NULL);
+  pthread_create(&consumer_tid, NULL, consumer_thread, &mailBox);
 
   // Join the thread
   pthread_join(sampler_tid, NULL);
